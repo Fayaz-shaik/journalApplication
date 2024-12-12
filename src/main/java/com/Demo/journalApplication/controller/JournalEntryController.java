7d1ad774c954c89fb76dfc5dfcdf414071143271
SHAIK FAYAZ <fayazshaik1722@gmail.com>
2024-12-12 18:33:33 +0530

    Updated the Service and Controller to Use the MongoDB and Added Lombok

--- src/main/java/com/Demo/journalApplication/controller/JournalEntryController.java
+++ src/main/java/com/Demo/journalApplication/controller/JournalEntryController.java
@@ -2,46 +2,73 @@ package com.Demo.journalApplication.controller;
 
 import com.Demo.journalApplication.entitiy.JournalEntry;
 import com.Demo.journalApplication.service.JournalEntryService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/_journal")
 public class JournalEntryController {
 
-    private final Map<Long, JournalEntry> journalEntries = new HashMap<>();
+//    private final Map<Long, JournalEntry> journalEntries = new HashMap<>();
     @Autowired
     private JournalEntryService journalEntryService;
 
-    @GetMapping()
-    public List<JournalEntry> getAll() {
-        return new ArrayList<>(journalEntries.values());
+    @GetMapping
+    public ResponseEntity<List<JournalEntry>> getAll() {
+        List<JournalEntry> all = journalEntryService.getAll();
+        if(all.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(all,HttpStatus.OK);
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry) {
-        journalEntryService.saveEntry(myEntry);
-        return true;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
     }
 
     @GetMapping("/id/{myId}")
-    public JournalEntry getJournalEntryByID(@PathVariable Long myId) {
-        return journalEntries.get(myId);
+    public ResponseEntity<JournalEntry> getJournalEntryByID(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = Optional.ofNullable(journalEntryService.findById(myId));
+        if(journalEntry.isPresent())
+        return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
     @DeleteMapping("/id/{myId}")
-    public JournalEntry deleteJournalEntryByID(@PathVariable Long myId) {
-        return journalEntries.remove(myId);
+    public ResponseEntity<?> deleteJournalEntryByID(@PathVariable ObjectId myId) {
+        if(journalEntryService.findById(myId)==null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        journalEntryService.deleteById(myId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
-        journalEntries.put(myId, myEntry);
-        return journalEntries.get(myId);
+    public ResponseEntity<JournalEntry> updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry myEntry) {
+
+        JournalEntry old = journalEntryService.findById(myId);
+        if(old!=null){
+            old.setTitle(myEntry.getTitle() != null && !myEntry.getTitle().isEmpty() ? myEntry.getTitle() : old.getTitle());
+            old.setContent(myEntry.getContent() != null && !myEntry.getContent().isEmpty() ? myEntry.getContent() : old.getContent());
+            old.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(old);
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/Demo/journalApplication/service/JournalEntryService.java
+++ src/main/java/com/Demo/journalApplication/service/JournalEntryService.java
@@ -3,6 +3,7 @@ package com.Demo.journalApplication.service;
 
 import com.Demo.journalApplication.entitiy.JournalEntry;
 import com.Demo.journalApplication.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,11 @@ public class JournalEntryService {
 
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
+    }
+    public JournalEntry findById(ObjectId id){
+        return journalEntryRepository.findById(id).orElse(null);
+    }
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
     }
 }
